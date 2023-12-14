1a7e776240429ac35c408510650c228acef14e76
Jordan <higgsfj@gmail.com>
2023-12-14 14:02:40 -0500

    Added multiplication method

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { increase, decrease } from './counterSlice'
+import { increase, decrease, multiply } from './counterSlice'
 
 function App() {
+
+  const [input, setInput] = useState(0)
+
 
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter)
   console.log(counter);
 
+  function handleChange(e) {
+    setInput(e.target.value)
+  }
 
   return (
     <div>
       <h3> Counter: {counter}</h3>
+      <input type="number" onChange={handleChange} value={input}/>
+      <br />
       <button onClick={() => dispatch(increase())}>Increase</button>
       <br /> <br />
       <button onClick={() => dispatch(decrease())}>Decrease</button>
+      <br /> <br />
+      <button onClick={() => dispatch(multiply(input))}>Times Input</button>
     </div>
   )
 }
--- src/counterSlice.js
+++ src/counterSlice.js
@@ -10,12 +10,15 @@ const counterSlice = createSlice({
         },
         decrease: (state) => {
             return state -= 1
+        },
+        multiply: (state, action) => {
+            return state * action.payload
         }
     }
 })
 
 
 
-export const { increase, decrease } = counterSlice.actions
+export const { increase, decrease, multiply } = counterSlice.actions
 
 export default counterSlice.reducer;
